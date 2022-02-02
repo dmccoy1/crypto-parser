ae52ca50ae2dc04dba61c67405227430a60e9cff
Darius McCoy <dariusmccoy@Betamores-MacBook-Air-2.local>
2022-02-02 18:17:04 -0500

    inital commit

--- etl.js
+++ etl.js
@@ -1,11 +1,16 @@
 const csv = require('csv-parser');
 const fs = require('fs');
-
+let a = []
 fs.createReadStream('coin_Bitcoin.csv')
     .pipe(csv())
     .on('data', (row) => {
-        console.log(row);
+        a.push(row);
     })
     .on('end', () => {
         console.log('CSV file successfully processed');
+        console.log(a[0], a[1], a[2], a[3], a[4])
+
     });
+//sleep(5000).then(() => {
+console.log(a)
+//})
